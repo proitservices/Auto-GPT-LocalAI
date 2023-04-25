b519411ea59d63948b8830d148b4905b139651da
proitservices <39563293+proitservices@users.noreply.github.com>
2023-04-25 20:17:41 +0100

    command to call local gpt4all

--- autogpt/commands/local_gpt.py
+++ autogpt/commands/local_gpt.py
@@ -1,17 +1,38 @@
+import json
 from typing import Any, Dict
+from urllib import request, parse
 from autogpt.commands.command import command
 from autogpt.config import Config
 
 CFG = Config()
 
+def call_local_gpt_api(name, task, prompt):
+    url = "http://localhost:8080/v1/completions"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "model": "ggml-gpt4all-j.bin",
+        "task": task,
+        "prompt": prompt,
+        "temperature": 0.7
+    }
+    data = json.dumps(data).encode("utf-8")
+
+    req = request.Request(url, headers=headers, data=data)
+    with request.urlopen(req) as response:
+        response_text = response.read().decode("utf-8")
+        reply = json.loads(response_text)
+        choices = reply.get("choices", [])
+        result = {"message": choices[0]["text"] if choices else ""}
+    return result
+
 @command(
     "local_gpt",
     "Local GPT",
     '"name": "agent_name_here", "task": "agent_task_here", "local_prompt": "message to the local agent"',
-    )
+)
 def local_gpt(name: str, task: str, local_prompt: str) -> Dict[str, Any]:
-    from .gpt_agent import main as gpt_agent_main
-    result = gpt_agent_main(name, task, local_prompt)
-    
-    return {"message": result}
-
+    try:
+        result = call_local_gpt_api(name, task, local_prompt)
+    except Exception as e:
+        result = {"message": f"Error: {str(e)}"}
+    return result
